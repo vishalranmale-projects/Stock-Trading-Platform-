0b401da682aed480cc1d2479da4e8dc1f83fea97
Vishal Ranmale <vishalranmale57@gmail.com>
2026-07-26 16:39:29 +0530

    Enhance Education and Pricing components with new layouts and content; add Pricing CSS styles

--- Frontend/landing_page/home/Education.jsx
+++ Frontend/landing_page/home/Education.jsx
@@ -1,8 +1,44 @@
 function Education() {
   return (
-    <button type="button" class="btn btn-primary">
-      Primary
-    </button>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-5">
+          <img src="/public/Media/education.svg"></img>
+        </div>
+        <div className="col-2"></div>
+        <div className="col-5">
+          <div className="row mb-5">
+            <h3>Free and open market education</h3>
+          </div>
+          <div className="row">
+            <p style={{ marginBottom: "2rem" }}>
+              Versity,the largest online stock market education bcovering
+              everything from the basics to advanced tr
+            </p>
+          </div>
+          <div className="row mb-5">
+            <a href="#" style={{ textDecoration: "none" }}>
+              Versity<i class="fa-solid fa-arrow-right-long"></i>
+            </a>
+          </div>
+          <div className="row">
+            <p>
+              Traiding Q&A,the most active traiding and investment community in
+              india for all your market related queries
+            </p>
+          </div>
+          <div className="row">
+            <a href="#" style={{ textDecoration: "none" }}>
+              TradingQ&A
+              <i
+                class="fa-solid fa-arrow-right-long"
+                style={{ textAlign: "center" }}
+              ></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- Frontend/landing_page/home/Pricing.jsx
+++ Frontend/landing_page/home/Pricing.jsx
@@ -1,5 +1,45 @@
+import "../../public/Stylesheets/Pricing.css";
 function Pricing() {
-  return <h1>Pricing</h1>;
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-5">
+          <div className="row">
+            <h2>Unbeatable pricing</h2>
+          </div>
+          <div className="row mt-4">
+            <p>
+              We pioneered the concept of discount broking and price
+              transparency in india. Flat fees and no hidden charges
+            </p>
+          </div>
+          <div className="row">
+            <a href="#" style={{ textDecoration: "none" }} className="mt-2">
+              See pricing&nbsp; <i class="fa-solid fa-arrow-right-long"></i>
+            </a>
+          </div>
+        </div>
+        <div className="col-1"></div>
+        <div className="col-6 mb-5">
+          <div className="row">
+            <div className="col innerBlocks">
+              <h3>
+                <i class="fa-solid fa-indian-rupee-sign rupeelogo"></i>0
+              </h3>
+              <p style={{ marginBottom: "0px" }}>free equity delivery and</p>{" "}
+              <p style={{ paddingTop: "0px" }}>direct mutual funds</p>
+            </div>
+            <div className="col innerBlocks">
+              <h3 style={{ marginBottom: "2rem" }}>
+                <i class="fa-solid fa-indian-rupee-sign rupeelogo"></i>20
+              </h3>
+              <p>Intraday and F&Q</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pricing;
