b489e70e39d5073d5e6cf1406768686a8a6e169b
Vishal Ranmale <vishalranmale57@gmail.com>
2026-07-31 07:20:56 +0530

    Initail Setup of An Dashboard

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -1,51 +1,110 @@
 import { useState } from "react";
 import React from "react";
+import { Link } from "react-router-dom";
 // import { handleProfileClick } from "../utils/someFile";
 const Menu = () => {
-  let [selectedMenue, setselectedMenue] = useState(0);
+  let [selectedMenue, setselectedMenue] = useState(1);
+  function handleClickMenue(e, a) {
+    setselectedMenue((selectedMenue) => {
+      return a;
+    });
+    console.log(selectedMenue + "hii");
+  }
   return (
     <div className="menu-container">
       <img src="logo.png" style={{ width: "40px" }} />
       <div className="menus">
         <ul>
-          <li>
-            <a href="/" style={{ textDecoration: "none", color: "black" }}>
+          <li data-value={1} onClick={(e) => handleClickMenue(e, 1)}>
+            <Link
+              to={"/"}
+              style={{
+                textDecoration: "none",
+                color: selectedMenue === 1 ? "rgb(14, 111, 201)" : "black",
+              }}
+            >
               <p>Dashboard</p>
-            </a>
+            </Link>
           </li>
-          <li>
-            <a
-              href="/orders"
-              style={{ textDecoration: "none", color: "black" }}
+          <li
+            value={1}
+            onClick={(e) => {
+              handleClickMenue(e, 2);
+            }}
+          >
+            <Link
+              to={"/orders"}
+              style={{
+                textDecoration: "none",
+                color: selectedMenue === 2 ? "rgb(14, 111, 201)" : "black",
+              }}
             >
               <p>Orders</p>
-            </a>
+            </Link>
           </li>
-          <li>
-            <a
-              href="/holdings"
-              style={{ textDecoration: "none", color: "black" }}
+          <li
+            value={3}
+            onClick={(e) => {
+              handleClickMenue(e, 3);
+            }}
+          >
+            <Link
+              to={"/holdings"}
+              style={{
+                textDecoration: "none",
+                color: selectedMenue === 3 ? "rgb(14, 111, 201)" : "black",
+              }}
             >
               <p>Holdings</p>
-            </a>
+            </Link>
           </li>
-          <li>
-            <a
-              href="positions"
-              style={{ textDecoration: "none", color: "black" }}
+          <li
+            value={4}
+            onClick={(e) => {
+              handleClickMenue(e, 4);
+            }}
+          >
+            <Link
+              to={"positions"}
+              style={{
+                textDecoration: "none",
+                color: selectedMenue === 4 ? "rgb(14, 111, 201)" : "black",
+              }}
             >
               <p>Positions</p>
-            </a>
+            </Link>
           </li>
-          <li>
-            <a href="funds" style={{ textDecoration: "none", color: "black" }}>
+          <li
+            value={5}
+            onClick={(e) => {
+              handleClickMenue(e, 5);
+            }}
+          >
+            <Link
+              to={"funds"}
+              style={{
+                textDecoration: "none",
+                color: selectedMenue === 5 ? "rgb(14, 111, 201)" : "black",
+              }}
+            >
               <p>Funds</p>
-            </a>
+            </Link>
           </li>
-          <li>
-            <a href="apps" style={{ textDecoration: "none", color: "black" }}>
+          <li
+            value={6}
+            onClick={(e) => {
+              handleClickMenue(e, 6);
+            }}
+          >
+            <Link
+              to={"apps"}
+              style={{
+                textDecoration: "none",
+                color: selectedMenue === 6 ? "rgb(14, 111, 201)" : "black",
+              }}
+            >
               <p>Apps</p>
-            </a>
+            </Link>
           </li>
         </ul>
         <hr />
