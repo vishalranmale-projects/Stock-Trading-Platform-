3ce3e7190e569d2b4325debf252fb8b9d13590de
Vishal Ranmale <vishalranmale57@gmail.com>
2026-08-08 19:28:32 +0530

    feat: replace anchor tags with Link components for navigation in Navbar

--- Frontend/landing_page/Navbar.jsx
+++ Frontend/landing_page/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "../public/Stylesheets/Navbar.css";
 import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 function Navbar() {
   let [click2, setclick] = useState(0);
   function handleClick(click, e) {
-    e.preventDefault();
     setclick(click);
   }
   return (
@@ -20,66 +20,66 @@ function Navbar() {
           ></img>
         </a>
         <div className="Navbar mt-2">
-          <a
+          <Link
+            to={"http://localhost:5173/signup"}
             style={{ color: click2 == 1 ? "rgb(14, 111, 201)" : "black" }}
             onClick={(e) => {
               handleClick(1, e);
             }}
             className="navbar-brand Navbartext"
-            href="http://localhost:5173/signup"
           >
             Signup
-          </a>
-          <a
+          </Link>
+          <Link
+            to={"http://localhost:5173/support"}
             style={{ color: click2 == 2 ? "rgb(14, 111, 201)" : "black" }}
             onClick={(e) => {
               handleClick(2, e);
             }}
             className="navbar-brand Navbartext"
-            href="http://localhost:5173/support"
           >
             Support
-          </a>
-          <a
+          </Link>
+          <Link
             style={{ color: click2 == 3 ? "rgb(14, 111, 201)" : "black" }}
             onClick={(e) => {
               handleClick(3, e);
             }}
             className="navbar-brand Navbartext"
-            href="http://localhost:5173/pricing"
+            to={"http://localhost:5173/pricing"}
           >
             Pricing
-          </a>
-          <a
+          </Link>
+          <Link
+            to={"http://localhost:5173/products"}
             style={{ color: click2 == 4 ? "rgb(14, 111, 201)" : "black" }}
             onClick={(e) => {
               handleClick(4, e);
             }}
             className="navbar-brand Navbartext"
-            href="http://localhost:5173/products"
           >
             Products
-          </a>
-          <a
+          </Link>
+          <Link
+            to={"http://localhost:5173/about"}
             style={{ color: click2 == 5 ? "rgb(14, 111, 201)" : "black" }}
             onClick={(e) => {
               handleClick(5, e);
             }}
             className="navbar-brand Navbartext"
-            href="http://localhost:5173/about"
           >
             About
-          </a>
-          <a
+          </Link>
+          <Link
             style={{ color: click2 == 6 ? "rgb(14, 111, 201)" : "black" }}
             className="navbar-brand Navbartext"
-            href="#"
+            to={"#"}
             onClick={(e) => {
               handleClick(6, e);
             }}
           >
             <i style={{ opacity: ".7" }} class="fa-solid fa-bars"></i>
-          </a>
+          </Link>
         </div>
       </div>
     </nav>
