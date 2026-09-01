67cd8c0adaba7f6892b72b15e43a986899c43bea
Vishal Ranmale <vishalranmale57@gmail.com>
2026-09-01 21:44:57 +0530

    Add urlencoded middleware to Express app

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ import passport from "passport";
 import passportLocal from "passport-local";
 import userModel from "./model/user.js";
 configDotenv();
+app.use(extended.urlencoded());
 const app = express();
 app.use(cors());
 const sessionOptions = {
